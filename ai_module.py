16b5470cbbfd43b6c9fae81c845bb89411ff3aec
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2021-02-06 13:27:05 +0500

    Update ai_module.py

--- ai_module.py
+++ ai_module.py
@@ -1,9 +1,12 @@
 def sayai(file):
-    if file == "How are you?":
+    if file == "Hi":
+        words = "How are you?"
+    elif file == "How are you?":
         words = "I am good! thank you."
     elif file == "Hello!":
         words = "How are you?"
     elif file == "I am good! thank you.":
         words = "Ok."
     return words
+
 __version__ = "0.1"
